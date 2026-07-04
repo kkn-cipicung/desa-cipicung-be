84f620525a92a688fd9b5aff7f5f2b1e10286faa
roby <robyaryanata12345@gmail.com>
2026-07-04 13:55:48 +0700

    feat: ci

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,7 +3,7 @@ package auth
 import "github.com/gin-gonic/gin"
 
 func (h *Handler) RegisterRoutes(router *gin.RouterGroup) {
-	auth := router.Group("/auth")
+	auth := router.Group("/admin/auth")
 	{
 		auth.POST("/register", h.Register)
 		auth.POST("/login", h.Login)
